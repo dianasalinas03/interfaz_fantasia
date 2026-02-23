6d7db8a8ffd94ac835dd35a7711931a01249bb14
Diana Salinas <diana_salinas03@hotmail.com>
2026-02-23 07:25:26 -0700

    Initial Commit

---
+++ interfaz_fantasia/interfaz_fantasiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  interfaz_fantasiaApp.swift
+//  interfaz_fantasia
+//
+//  Created by Diana⭐ on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct interfaz_fantasiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
